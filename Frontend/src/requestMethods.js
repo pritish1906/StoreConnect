6c232c4de3390593db1bae889358428d4e107967
pritishsharma96@gmail.com <pritishsharma96@gmail.com>
2023-07-04 01:06:03 +0530

    dashboard-added

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const BASE_URL = "http://localhost:5000/api/"
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGNhOTgyMzA3NjQ0NTI4YzY1NmY2NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4Nzk4NjM0MCwiZXhwIjoxNjg4MDcyNzQwfQ.z4y7lztU68TYG8bgBfOsiKUhe_8ibjgI071dR-xg-c8"
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
@@ -11,5 +11,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    header:{token: `Bearer ${TOKEN}`}
+    headers:{token: `Bearer ${TOKEN}`}
 })
